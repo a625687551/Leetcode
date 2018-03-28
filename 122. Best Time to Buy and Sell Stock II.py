628ecc65f6993281e636ac398574af1bf0dcf328
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2018-03-28 13:59:34 +0800

    add 108 and 122

---
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,10 @@
+# ！/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
