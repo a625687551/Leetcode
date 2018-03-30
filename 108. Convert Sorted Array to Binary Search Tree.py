be08bc0851557fdcd7f2584f7717e07e0ba9a46b
rising <625687551@qq.com>
2018-03-30 08:41:18 +0800

    change some syntax error

--- 108. Convert Sorted Array to Binary Search Tree.py
+++ 108. Convert Sorted Array to Binary Search Tree.py
@@ -3,11 +3,12 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     def sortedArrayToBST(self, nums):
@@ -17,15 +18,16 @@ class Solution:
         """
         if not nums:
             return None
-        
+
         mid = len(nums) // 2
         root = TreeNode(nums[mid])
         root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
 
         return root
 
+
 if __name__ == '__main__':
-	nums = [-10,-3,0,5,9]
-	s = Solution()
-	print(s.sortedArrayToBST(nums))
+    nums = [-10, -3, 0, 5, 9]
+    s = Solution()
+    print(s.sortedArrayToBST(nums))
