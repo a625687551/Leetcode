da1c9c5aac0847237c26dddfc1057b886110ca95
rising <625687551@qq.com>
2018-04-09 08:17:57 +0800

    add 3

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,3 +1,20 @@
 # !/usr/bin/env python
 # coding: utf-8
 
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = max_length = 0
+        used = {}
+
+        for i, c in enumerate(s):
+            if c in used and start <= used[c]:
+                start = used[c] + 1
+            else:
+                max_length = max(max_length, i-start+1)
+            used[c] = i
+        return max_length
--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,3 +1,23 @@
 # !/usr/bin/env python
 # coding: utf-8
 
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        length = len(s)
+        if length<2:
+            return s
+        res = ""
+        for i in range(length):
+            res = max(self.helper(s,i,i), self.helper(s,i,i+1),res, key=len)
+        return res
+
+    def helper(self, s, l, r):
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
