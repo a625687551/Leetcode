27f3f468128ba37195adae40149369036ea31e0c
rising <625687551@qq.com>
2018-06-26 08:19:01 +0800

    finish define linked list

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -7,46 +7,195 @@ class Node(object):
     _next:保存下一个节点对象
     """
 
-    def ___init__(self, data, pnext=None):
+    def __init__(self, data):
         self.data = data
-        self._next = pnext
+        self._next = None
 
     def __repr__(self):
         """
         用来定义Node的字符输出
-        print为输出data
+        Returns:
+            字符串输出
         """
         return str(self.data)
 
-    def __getitem__(self, index):
-        pass
 
-    def __setitem__(self, ind, val):
-        pass
-
-    def __len__(self):
-        return self.length
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
 
     def is_empty(self):
-        return (self.length == 0)
+        return self.length == 0
 
     def append(self, data):
-        pass
+        """"
+        在链表末尾添加node值，如果是node对象直接添加，否则先转换为node对象
+        Args:
+            data：数据或者node对象
+        Returns:
+            None
+        """
+        if isinstance(data, Node):
+            item = data
+        else:
+            item = Node(data)
+        if not self.head:
+            self.head = item
+        else:
+            node = self.head
+            while node._next:
+                node = node._next
+            node._next = item
+        self.length += 1
 
     def delete(self, index):
-        pass
+        """
+        删除链表中某个位置的节点
+        Args:
+            index: 位置索引
+        Returns:
+            None
+        """
+        if isinstance(index, int):
+            if index > self.length:
+                print("index is out of range")
+                return
+            else:
+                if index == 0:
+                    self.head = self.head._next
+                else:
+                    current_node = self.head
+                    while index - 1:
+                        current_node = current_node._next
+                        index -= 1
+                    current_node._next = current_node._next._next
+                self.length -= 1
+                return
+        else:
+            print("index value is not int")
+            return
 
-    def insert(self, index, data_or_node):
-        pass
+    def insert(self, index, value):
+        """
+        链表的插入操作
+        Args:
+            value: 要插入的值
+            index: 位置索引
+        Returns:
+            None
+        """
+        if isinstance(index, int):
+            if index > self.length:
+                print("index is out of range")
+                return
+            else:
+                this_node = Node(data=value)
+                current_node = self.head
 
-    def update(self, index, data):
-        pass
+                if index == 0:
+                    self.head = this_node
+                    this_node._next = current_node
+                    return
+                while index - 1:
+                    current_node = current_node._next
+                    index -= 1
+                this_node._next = current_node._next
+                current_node._next = this_node
+                self.length += 1
+                return
+        else:
+            print("index value is not int")
+            return
 
-    def get_item(self, index):
-        pass
+    def update(self, index, value):
+        """
+        为链表中某个位置的节点修改值
+        Args:
+            value: 要修改的值
+            ndex: 位置索引
+        Returns:
+            None
+        """
+        if isinstance(index, int):
+            if index > self.length:
+                print("index is out of range")
+                return
+            else:
+                this_node = Node(data=value)
 
-    def get_index(self, data):
-        pass
+                if index == 0:
+                    this_node._next = self.head
+                    self.head = this_node
+                else:
+                    current_node = self.head
+                    while index - 1:
+                        current_node = current_node._next
+                        index -= 1
+                    this_node._next = current_node._next._next
+                    current_node._next = this_node
+        else:
+            print("index value is not int")
+            return
+
+    def get_value(self, index):
+        """
+        获取链表中某个位置节点的值
+        Args:
+            index: 位置索引
+        Returns:
+             该节点值
+        """
+        if isinstance(index, int):
+            if index > self.length:
+                print("index is out of range")
+                return
+            else:
+                if index == 0:
+                    return self.head.data
+                else:
+                    current_node = self.head
+                    while index - 1:
+                        current_node = current_node._next
+                        index -= 1
+                    return current_node._next.data
+        else:
+            print("index is not int")
+            return
 
     def clear(self):
-        pass
+        """
+        清空链表
+        """
+        self.head = None
+        self.length = 0
+        print("clear the linkedlist finished")
+
+    def print_linked_list(self):
+        """
+        对整个链表打印
+        """
+        if self.is_empty():
+            print("linked list length is 0")
+        else:
+            node = self.head
+            print("Head -->", node.data, end=" ")
+            while node._next:
+                node = node._next
+                print("-->", node.data, end=" ")
+            print("--> None, Linked list finish")
+
+
+if __name__ == '__main__':
+    n1 = Node(data="node1")
+    n2 = Node(data="node2")
+    l = LinkedList()
+    l.append(n1)
+    l.append(n2)
+    l.print_linked_list()
+    l.insert(1, "mao")
+    l.print_linked_list()
+    l.update(index=2, value="chong")
+    l.print_linked_list()
+    print(l.get_value(index=2))
+    print(l.length)
