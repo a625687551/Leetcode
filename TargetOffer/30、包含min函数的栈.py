8cc5856f7d0ad2e034504e9047ae2b112096872e
rising <625687551@qq.com>
2018-09-06 08:38:36 +0800

    add two problem

---
+++ TargetOffer/30、包含min函数的栈.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+"""
+定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+"""
+
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, node):
+        self.stack.append(node)
+        if not self.min_stack or self.min_stack[-1] > node:
+            self.min_stack.append(node)
+        else:
+            self.min_stack.append(self.min_stack[-1])
+
+    def pop(self):
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def min(self):
+        return self.min_stack[-1]
+
+
+s = Solution()
+s.push(3)
+print(s.min())
+print(s.stack, s.min_stack)
+s.push(4)
+print(s.min())
+print(s.stack, s.min_stack)
+s.push(2)
+print(s.min())
+print(s.stack, s.min_stack)
+s.push(3)
+print(s.min())
+s.pop()
+print(s.min())
