1274de6ff4fd1d265e0f402e335c2308a02a2fff
rising <625687551@qq.com>
2018-09-20 22:18:11 +0800

    add 3 problem

--- TargetOffer/64、求1+2+3+...+n.py
+++ TargetOffer/64、求1+2+3+...+n.py
@@ -1,9 +1,19 @@
 # -*- coding:utf-8 -*-
 """
 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+思路：
+1、递归的调用加函数
+2、虚函数求解
+3、函数指针求解
+4、模板类型求解
 """
 
 
 class Solution:
     def Sum_Solution(self, n):
-        pass
+        result = (n > 0) and (n + self.Sum_Solution(n - 1))
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.Sum_Solution(10))
