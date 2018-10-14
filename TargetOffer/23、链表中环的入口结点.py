7653a59d6c1b05049aec09856a3213ced9d6471e
rising <625687551@qq.com>
2018-10-14 08:48:25 +0800

    chang some name and add list

---
+++ TargetOffer/23、链表中环的入口结点.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+"""
+给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+.此时慢指针走的路程为Sslow = x + m * c + a
+快指针走的路程为Sfast = x + n * c + a
+2 Sslow = Sfast
+2 * ( x + m*c + a ) = (x + n *c + a)
+从而可以推导出：
+x = (n - 2 * m )*c - a
+= (n - 2 *m -1 )*c + c - a
+即环前面的路程 = 数个环的长度（为可能为0） + c - a
+什么是c - a？这是相遇点后，环后面部分的路程。（橙色路程）
+所以，我们可以让一个指针从起点A开始走，让一个指针从相遇点B开始继续往后走，
+2个指针速度一样，那么，当从原点的指针走到环入口点的时候（此时刚好走了x）
+从相遇点开始走的那个指针也一定刚好到达环入口点。
+所以2者会相遇，且恰好相遇在环的入口点。
+最后，判断是否有环，且找环的算法复杂度为：
+时间复杂度：O(n)
+空间复杂度：O(1)
+"""
+
+
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        if not pHead or not pHead.next:
+            return None
+        fast = pHead.next.next
+        slow = pHead.next
+        while fast != slow:
+            if fast.next and fast.next.next:
+                fast = fast.next.next
+                slow = slow.next
+            else:
+                return None
+        fast = pHead
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        return slow
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.Power(2, 5))
---
+++ TargetOffer/52、两个链表的第一个公共结点.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+"""
+输入两个链表，找出它们的第一个公共结点。
+1.先遍历list 放到两个辅助栈中，每次弹出一个，对比，时间复杂度和空间复杂度是O(m+n)
+2.先遍历list记住长度m n，长的先走几步对齐，然后在逐个对比，时间复杂度O(m+n)空间复杂度1
+3、用两个指针扫描”两个链表“，最终两个指针到达 null 或者到达公共结点。不用记住长度
+"""
+
+
+class Solution:
+    def FindFirstCommonNode(self, pHead1, pHead2):
+        p1 = pHead1
+        p2 = pHead2
+        while p1 != p2:
+            p1 = pHead2 if not p1 else p1.next
+            p2 = pHead1 if not p2 else p2.next
+
+        return p1
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.Power(2, 5))
