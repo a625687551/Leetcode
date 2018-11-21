1b1d39cc6294a493365bdc986616b4b8fa04ca63
rising <625687551@qq.com>
2018-11-21 22:07:48 +0800

    finish 3

--- TargetOffer/14、剪绳子.py
+++ TargetOffer/14、剪绳子.py
@@ -1,16 +1,45 @@
 # -*- coding:utf-8 -*-
 """
-请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，
-而'*'表示它前面的字符可以出现任意次（包含0次）。 在本题中，匹配是指字符串的所有字符匹配整个模式。
-例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
+给你一根长度为n的绳子，请把绳子剪成m段 (m和n都是整数，n>1并且m>1)每段绳子的长度记为k[0],k[1],…,k[m].
+请问k[0]k[1]…*k[m]可能的最大乘积是多少？例如，当绳子的长度为8时，我们把它剪成长度分别为2,3,3的三段，此时得到的最大乘积是18.
 """
 
 
 class Solution:
-    def match(self, s, pattern):
-        return False
+    def max_cut_1(self, s_length):
+        """动态规划"""
+        if s_length < 2:
+            return 0
+        if s_length == 2:
+            return 1
+        if s_length == 3:
+            return 2
+        li = [0, 1, 2, 3]
+        for i in range(4, s_length + 1):
+            max_num = 0
+            for j in range(1, i):
+                temp = li[j] * li[i - j]
+                if temp > max_num:
+                    max_num = temp
+            li.append(max_num)
+        return li[-1]
 
+    def max_cut_2(self, s_length):
+        """贪婪算法，当n>=5时候尽可能的切成长度为3的绳子，n=4的时候尽可能切成2*2的绳子"""
+        if s_length < 2:
+            return 0
+        if s_length == 2:
+            return 1
+        if s_length == 3:
+            return 2
+        time_three = s_length // 3
+
+        if s_length - time_three*3 == 1:
+            time_three -= 1
+        time_two = (s_length - time_three*3) // 2
+        return 3**time_three*2**time_two
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.match("a", ".*"))
+    print(s.max_cut_1(8))
+    print(s.max_cut_2(8))
--- TargetOffer/17、打印从1到最大的n位数.py
+++ TargetOffer/17、打印从1到最大的n位数.py
@@ -7,10 +7,87 @@
 
 
 class Solution:
-    def match(self, s, pattern):
-        return False
+    def print_n(self, n):
+        if n <= 0:
+            return
+        list_num = ["0"] * n
+        while self.increament(list_num) is False:
+            self.print_num(list_num)
+
+    def print_num(self, num):
+        is_begin = False
+        for i in range(len(num)):
+            if num[i] != "0" and is_begin is False:
+                is_begin = True
+            if is_begin:
+                tmp = ("".join(num[i:]))
+                print(tmp)
+                break
+
+    def increament(self, num):
+        is_overflow = False
+        is_incre = 0  # 是否能够归零进一
+        len_num = len(num)
+        n = len_num - 1
+        while n >= 0:
+            nsum = int(num[n]) + is_incre
+            if n == len_num - 1:
+                nsum += 1
+            if nsum >= 10:
+                if n == 0:
+                    is_overflow = True
+                else:
+                    is_incre = 1
+                    nsum = nsum % 10
+                    num[n] = str(nsum)
+            else:
+                num[n] = str(nsum)
+            n -= 1
+        return is_overflow
+
+class Solution1:
+    def Increment(self, number):
+        isOverflow = False
+        flag = 0
+        lenth = len(number)
+        n = lenth - 1
+        while n >= 0:
+            nSum = int(number[n]) + flag
+            if n == lenth - 1:
+                nSum += 1
+            if nSum >= 10:
+                if n == 0:
+                    isOverflow = True
+                else:
+                    flag = 1
+                    nSum = nSum % 10
+                    number[n] = str(nSum)
+            else:
+                number[n] = str(nSum)
+            n -= 1
+        return isOverflow
+
+    def PrintNumber(self, number):
+        isBegin = False
+        lenth = len(number)
+        for i in range(lenth):
+            if number[i] != '0' and isBegin is False:
+                isBegin = True
+            if isBegin:
+                tmp = ("".join(number[i:]))
+                print(tmp)
+                break
+
+    def Print1ToMaxOfNDigits(self, n):
+        if n <= 0:
+            return
+        number = ['0'] * n
+        while self.Increment(number) is False:
+            self.PrintNumber(number)
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.match("a", ".*"))
+    t = Solution1()
+    print(s.print_n(3))
+    # print(t.Print1ToMaxOfNDigits(3))
