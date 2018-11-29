506033f088606f689fb898a2d687d445518a507a
rising <625687551@qq.com>
2018-11-29 08:15:48 +0800

    change some for 51-60

--- TargetOffer/58、翻转字符串.py
+++ TargetOffer/58、翻转字符串.py
@@ -1,12 +1,19 @@
 # -*- coding:utf-8 -*-
 """
-汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。
+一、例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。
+Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+二、汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。
 对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。例如，字符序列S=”abcXYZdef”,
 要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，搞定它！
 """
 
 
 class Solution:
+    def rotate_string(self, s):
+        if not s:
+            return None
+        return " ".join(s.split(" ")[::-1])
+
     def LeftRotateString(self, s, n):
         if not s:
             return ""
@@ -27,4 +34,5 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.LeftRotateString("abcdefg", 7))
+    # print(s.LeftRotateString("abcdefg", 7))
+    print(s.rotate_string("student. a am I"))
--- TargetOffer/59、队列最大值.py
+++ TargetOffer/59、队列最大值.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
 
 """
+一、滑动窗口的最大值
 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。
 例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，
 他们的最大值分别为{4,4,6,6,6,5}； 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个：
 {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}， 
 {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
+二、队列的最大值
 """
 
 
@@ -23,9 +25,11 @@ class Solution:
             res.append(num[index[0]])
             while len(index) > 0 and num[i] >= num[index[-1]]:
                 index.pop()
-            if len(index) > 0 and index[0] <= (i-size):
+            if len(index) > 0 and index[0] <= (i - size):
                 index.pop(0)
             index.append(i)
         res.append(num[index[0]])
         return res
 
+    def max_in_queue(self, num):
+        pass
