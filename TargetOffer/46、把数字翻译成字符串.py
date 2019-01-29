54f678d5d5384bb9180e9c472c414b4d38a8df43
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2019-01-29 17:57:50 +0800

    change digit to str

--- TargetOffer/46、把数字翻译成字符串.py
+++ TargetOffer/46、把数字翻译成字符串.py
@@ -3,7 +3,7 @@
 给定一个数字，按照如下规则翻译成字符串：0翻译成“a”，1翻译成“b”...25翻译成“z”。一个数字有多种翻译可能，
 例如12258一共有5种，分别是bccfi，bwfi，bczi，mcfi，mzi。实现一个函数，用来计算一个数字有多少种不同的翻译方法。
 动态规划问题
-12258
+                     12258
                    /       \
               b+2258       m+258
               /   \         /   \
@@ -14,7 +14,19 @@
    bccf+8        bczi    bwfi   mcfi
      /
  bccfi
-
+自下而上，动态规划，从最小的问题开始 ：
+f(r)表示以r为开始（r最小取0）到最右端所组成的数字能够翻译成字符串的种数。对于长度为n的数字，f(n)=0,f(n-1)=1,求f(0)。
+递推公式为 f(r-2) = f(r-1)+g(r-2,r-1)*f(r)；
+其中，如果r-2，r-1能够翻译成字符，则g(r-2,r-1)=1，否则为0。
+因此，对于12258：
+f(5) = 0
+f(4) = 1
+f(3) = f(4)+0 = 1
+f(2) = f(3)+f(4) = 2
+f(1) = f(2)+f(3) = 3
+f(0) = f(1)+f(2) = 5
+作者：ryderchan
+链接：https://www.jianshu.com/p/80e1841909b7
 """
 
 
@@ -23,11 +35,13 @@ class Solution:
         if numbers < 0:
             return 0
         str_num = str(numbers)
+        print("old", self.get_trans_count(str_num))
+        print("new", self.get_trans_count_2(str_num))
         return self.get_trans_count(str_num)
 
     def get_trans_count(self, str_num):
         length = len(str_num)
-        counts = [0]*length
+        counts = [0] * length
         for i in range(length - 1, -1, -1):
             if i < length - 1:
                 count = counts[i + 1]
@@ -48,7 +62,23 @@ class Solution:
         # print(counts)
         return counts[0]
 
+    def get_trans_count_2(self, str_num):
+        length = len(str_num)
+        res = [0]*(length-1) + [1, 1]
+        for i in range(length - 2, -1, -1):
+            if int("{}{}".format(str_num[i], str_num[i+1])) < 26:
+                g = 1
+            else:
+                g = 0
+            # temp = f2
+            # f2 = temp + g*f1
+            # f1 = temp
+            res[i] = res[i+1] + res[i+2]*g
+        return res[0]
+
 
 if __name__ == '__main__':
     s = Solution()
     print(s.trans_num_str(12258))
+    print(s.trans_num_str(12858))
+    print(s.trans_num_str(12818))
---
+++ TargetOffer/72.DNA合成.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+链接：https://www.nowcoder.com/questionTerminal/e95337f886f54110b92318f693cd8fad
+来源：牛客网
+DNA分子是以4种脱氧核苷酸为单位连接而成的长链，这4种脱氧核苷酸分别含有A,T,C,G四种碱基。碱基互补配对原则：A和T是配对的，
+C和G是配对的。如果两条碱基链长度是相同的并且每个位置的碱基是配对的，那么他们就可以配对合成为DNA的双螺旋结构。
+现在给出两条碱基链，允许在其中一条上做替换操作：把序列上的某个位置的碱基更换为另外一种碱基。
+问最少需要多少次让两条碱基链配对成功
+"""
+
+
+class Solution(object):
+    def solution(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
