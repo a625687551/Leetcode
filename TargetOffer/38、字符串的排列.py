3e076d91ae6593a228d55f83eba3813a0da75e0f
rising <625687551@qq.com>
2019-03-01 08:24:19 +0800

    not finish 38

--- TargetOffer/38、字符串的排列.py
+++ TargetOffer/38、字符串的排列.py
@@ -22,6 +22,28 @@ class Solution:
             for i in range(len(ss)):
                 self.perm(ss[:i] + ss[i + 1:], res, path + ss[i])
 
+
+class Solution1:
+    """利用青蛙跳台阶的效应一样"""
+
+    def permutation(self, ss):
+        if not ss:
+            return
+        self.perm(ss, ss)
+
+    def perm(self, ss, begin):
+        if not begin:
+            print(ss)
+        else:
+            for i in ss:
+                temp = i
+                i = begin
+                begin = temp
+                self.perm(ss, )
+
+
 if __name__ == '__main__':
-    s = Solution()
-    print(s.Permutation("abc"))
+    # s = Solution()
+    # print(s.Permutation("abc"))
+    s = Solution1()
+    print(s.permutation("abc"))
